7d0a99ccd29bacae4c1e4d185c23ea2b7aaabad2
GaciuRadu <gaciuradu@gmail.com>
2023-08-21 15:27:06 +0300

    component as arrow function (exemple)

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,19 +1,17 @@
 import { TeamsTable } from "../teams/TeamsTable";
 
-function Toolbar() {
-  return (
-    <div className="tbar">
-      <div>
-        <button id="removeSelected">❌ Remove selected</button>
-      </div>
-      <div className="tfill"></div>
-      <div>
-        <input placeholder="Search" type="search" name="search" id="search" />
-        <label htmlFor="search"> 🔎 </label>
-      </div>
+const Toolbar = () => (
+  <div className="tbar">
+    <div>
+      <button id="removeSelected">❌ Remove selected</button>
     </div>
-  );
-}
+    <div className="tfill"></div>
+    <div>
+      <input placeholder="Search" type="search" name="search" id="search" />
+      <label htmlFor="search"> 🔎 </label>
+    </div>
+  </div>
+);
 
 export function ContentWrapper() {
   return (
