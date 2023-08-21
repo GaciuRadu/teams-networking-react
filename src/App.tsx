2ed08e8fc0b8f80a21fc7e194ca1de81a74f4437
GaciuRadu <gaciuradu@gmail.com>
2023-08-21 09:51:10 +0300

    add ContentWrapper and Toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -35,8 +35,29 @@ function AppHeader() {
     </header>
   );
 }
+
+function Toolbar() {
+  return (
+    <div className="tbar">
+      <div>
+        <button id="removeSelected">❌ Remove selected</button>
+      </div>
+      <div className="tfill"></div>
+      <div>
+        <input placeholder="Search" type="search" name="search" id="search" />
+        <label htmlFor="search"> 🔎 </label>
+      </div>
+    </div>
+  );
+}
+
 function ContentWrapper() {
-  return <section id="main">...</section>;
+  return (
+    <section id="main">
+      <Toolbar />
+      TODO table...
+    </section>
+  );
 }
 
 function AppFooter() {
