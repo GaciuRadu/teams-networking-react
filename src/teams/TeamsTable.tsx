c27287ba602bcc0139a5b451062eb176307478f1
GaciuRadu <gaciuradu@gmail.com>
2023-08-21 13:31:23 +0300

    map teams to html row

--- src/teams/TeamsTable.tsx
+++ src/teams/TeamsTable.tsx
@@ -1,4 +1,88 @@
+function TeamRow(team) {
+  const { id, url } = team;
+  const displayUrl = url.startsWith("https://github.com/") ? url.substring(19) : url;
+
+  return (
+    <tr>
+      <td style={{ textAlign: "center" }}>
+        <input type="checkbox" name="selected" value="${id}" />
+      </td>
+      <td>{team.promotion}</td>
+      <td>{team.members}</td>
+      <td>{team.name}</td>
+      <td>
+        <a href="${url}" target="_blank">
+          {" "}
+          {displayUrl}{" "}
+        </a>
+      </td>
+      <td>
+        <button data-id="${id}" type="button" className="action-btn edit-btn" title="edit">
+          ✎
+        </button>
+        <button data-id="${id}" type="button" className="action-btn delete-btn" title="delete">
+          🗑️
+        </button>
+      </td>
+    </tr>
+  );
+}
+
 export function TeamsTable() {
+  const teams = [
+    {
+      id: "f5rvqm1689178295237",
+      promotion: "Html",
+      members: "Gaciu Radu",
+      name: "Teams",
+      url: "https://github.com/GaciuRadu/teams-networking"
+    },
+    {
+      id: "fcrsi71691171132868",
+      promotion: "---REACT",
+      members: "Gaciu Radu",
+      name: "Teams",
+      url: "https://github.com/GaciuRadu/teams-networking"
+    },
+    {
+      id: "4cvef1691171145666",
+      promotion: "js",
+      members: "FastTrackIT",
+      name: "Web",
+      url: "https://github.com/nmatei/teams-networking-react"
+    },
+    {
+      id: "r74q2q1691588184869",
+      promotion: "☺️☺️css",
+      members: "Gaciu Radu",
+      name: "Teams",
+      url: "https://github.com/nmatei/teams-networking-react"
+    },
+    {
+      id: "5w6n4m1691588197285",
+      promotion: "REACT",
+      members: "Gaciu Radu",
+      name: "Teams",
+      url: "https://github.com/GaciuRadu/teams-networking"
+    },
+    {
+      id: "6ivdo1691588209551",
+      promotion: "js1",
+      members: "FastTrackIT",
+      name: "Web",
+      url: "https://github.com/nmatei/teams-networking-react"
+    },
+    {
+      id: "2epjvt1691593956038",
+      promotion: "html",
+      members: "Gaciu Radu",
+      name: "Web",
+      url: "https://github.com/nmatei/teams-networking-react"
+    }
+  ];
+
+  console.info("render table", teams);
+
   return (
     <form id="teamsForm" action="" method="get">
       <table id="teamsTable">
@@ -22,7 +106,7 @@ export function TeamsTable() {
             <th></th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>{teams.map(team => TeamRow(team))}</tbody>
         <tfoot>
           <tr>
             <td></td>
